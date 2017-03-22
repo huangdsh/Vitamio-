f767458a08e0ffef9f50748bee046e4a4428e8ec
hds <185469580@qq.com>
2017-03-22 15:06:30 +0800

    Vitamio的简单Demo

---
+++ app/src/main/java/com/fuicuiedu/demo/video_demo/MainActivity.java
@@ -0,0 +1,70 @@
+package com.fuicuiedu.demo.video_demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.fuicuiedu.demo.video_demo.DemoA.DemoAActivity;
+import com.fuicuiedu.demo.video_demo.DemoC.DemoCActivity;
+import com.fuicuiedu.demo.video_demo.DemoB.DemoBActivity;
+import com.fuicuiedu.demo.video_demo.DemoD.DemoDActivity;
+import com.fuicuiedu.demo.video_demo.DemoE.DemoEActivity;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    private ListView mlv;
+    private String[] datas;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mlv = (ListView) findViewById(R.id.main_lv);
+
+        datas = new String[]{
+                "Android MediaPlayer + SurfaceView/TextureView",
+                "Android VideoView + MediaController",
+                "Vitamio MediaPlayer + SurfaceView/TextureView",
+                "Vitamio VideoView + MediaController",
+                "Vitamio中VideoView Buffer 缓冲处理"
+        };
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datas);
+        mlv.setAdapter(adapter);
+
+        mlv.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent;
+        switch (position){
+            case 0:
+                intent = new Intent(MainActivity.this, DemoAActivity.class);
+                startActivity(intent);
+                break;
+            case 1:
+                intent = new Intent(MainActivity.this, DemoBActivity.class);
+                startActivity(intent);
+                break;
+            case 2:
+                intent = new Intent(MainActivity.this, DemoCActivity.class);
+                startActivity(intent);
+                break;
+            case 3:
+                intent = new Intent(MainActivity.this, DemoDActivity.class);
+                startActivity(intent);
+                break;
+            case 4:
+                intent = new Intent(MainActivity.this, DemoEActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
